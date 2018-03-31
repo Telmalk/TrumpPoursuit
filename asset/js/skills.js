179267b37d934340c9a43d87879a6b00c6042e05
mallano <maelallano1@gmail.com>
2018-03-31 10:31:44 +0200

    premier skill melania

--- asset/js/skills.js
+++ asset/js/skills.js
@@ -6,57 +6,109 @@ var melaniaBtn2 = document.querySelector('.MelaniaRangeBtn');
 // var trumpImg = document.querySelector('.trumpImg');
 var trumpBtn1 = document.querySelector('.TrumpCacBtn');
 var trumpBtn2 = document.querySelector('.TrumpRangeBtn');
+
+var HPTrump = document.querySelector(".HPTrump");
+var HPMelania = document.querySelector(".HPMelania");
+
 var pose = '';
 
-// melaniaBtn1.addEventListener('click', function() {
-//   melaniaCac();
-// });
+melaniaBtn1.addEventListener('click', function() {
+  melaniaCac();
+});
 
-// melaniaBtn2.addEventListener('click', function() {
-//   melaniaRange();
-// });
+melaniaBtn2.addEventListener('click', function() {
+  melaniaRange();
+});
 
-// trumpBtn1.addEventListener('click', function() {
-//   trumpCac();
-// });
+trumpBtn1.addEventListener('click', function() {
+  trumpCac();
+});
 
-// trumpBtn2.addEventListener('click', function() {
-//   trumpRange();
-// });
+trumpBtn2.addEventListener('click', function() {
+  trumpRange();
+});
 
+// function comparePos(el1, el2, el3, el4) {
+//   // Retourne true si les deux elements se trouvent sur une case adjacente
+//   // var pose contient le côté où se trouve l'élément comparé
+//   if (el1.posX - 1 == el2.posX && el3.posY == el4.posY) {
+//     pose = 'left';
+//     return true;
+//   }
+//   if (el1.posX + 1 == el2.posX && el3.posY == el4.posY) {
+//     pose = 'right';
+//     return true;
+//   }
+//   if (el1.posX == el2.posX && el3.posY - 1 == el4.posY) {
+//     pose = 'bottom';
+//     return true;
+//   }
+//   if (el1.posX == el2.posX && el3.posY + 1 == el4.posY) {
+//     pose = 'top';
+//     return true;
+//   }
+//   return false;
+// }
 function comparePos(el1, el2, el3, el4) {
   // Retourne true si les deux elements se trouvent sur une case adjacente
   // var pose contient le côté où se trouve l'élément comparé
-  if (el1.posX - 1 == el2.posX && el3.posY == el4.posY) {
-    pose = 'left';
-    return true;
+  if (turn % 2 === 1) {
+    if (el1 - 1 == el2 && el3 == el4) {
+      pose = 'left';
+      return true;
+    }
+    if (el1 + 1 == el2 && el3 == el4) {
+      pose = 'right';
+      return true;
+    }
+    if (el1 == el2 && el3 + 1 == el4) {
+      pose = 'bottom';
+      return true;
+    }
+    if (el1 == el2 && el3 - 1 == el4) {
+      pose = 'top';
+      return true;
+    }
   }
-  if (el1.posX + 1 == el2.posX && el3.posY == el4.posY) {
-    pose = 'right';
-    return true;
-  }
-  if (el1.posX == el2.posX && el3.posY - 1 == el4.posY) {
-    pose = 'bottom';
-    return true;
-  }
-  if (el1.posX == el2.posX && el3.posY + 1 == el4.posY) {
-    pose = 'top';}
-    return true;
+  return false;
 }
 
+// function melaniaCac() {
+//   comparePos(posXMela, posXTrump, posYMela, posYTrump);
+//   if (true) {
+//     datas.hpTrump -= 20;
+//     if (pose == 'left') {
+//       datas.posXtrump--;
+//     } else if (pose == 'right') {
+//       datas.posXTrump++;
+//     } else if (pose == 'top') {
+//       datas.posYMela--;
+//     } else {
+//       datas.posYMela++;
+//     }
+
+//   } else {
+//     // Faire l'anim d'action impossible
+//     melaniaBtn1.className = 'nope';
+//   }
+// }
+
 function melaniaCac() {
-  comparePos(posXMela, posXTrump, posYMela, posYTrump);
-  if (true) {
+  if (comparePos(melaniaPosX, trumpPosX, melaniaPosY, trumpPosY)) {
     datas.hpTrump -= 20;
+    hpTrump -= 20;
+    // HPTrump.innerHTML = hpTrump;
     if (pose == 'left') {
-      datas.posXtrump--;
+      trumpPosX--;
     } else if (pose == 'right') {
-      datas.posXTrump++;
+      trumpPosX++;
     } else if (pose == 'top') {
-      datas.posYMela--;
+      trumpPosY--;
     } else {
-      datas.posYMela++;
+      trumpPosY++;
     }
+
+    render();
 
   } else {
     // Faire l'anim d'action impossible
