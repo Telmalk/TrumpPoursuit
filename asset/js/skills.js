9ba2053768d1cbde1482bd3094799363ce667aad
Geoffrey28 <geoffreygirard28@gmail.com>
2018-03-31 03:53:11 +0200

    Skills correction

--- asset/js/skills.js
+++ asset/js/skills.js
@@ -1,10 +1,9 @@
 var myTable = document.getElementById('myTable');
-
 var melaniaBtn1 = document.querySelector('.MelaniaCacBtn');
 var melaniaBtn2 = document.querySelector('.MelaniaRangeBtn');
 var trumpBtn1 = document.querySelector('.TrumpCacBtn');
 var trumpBtn2 = document.querySelector('.TrumpRangeBtn');
-  var pose = '';
+var pose = '';
 
 melaniaBtn1.addEventListener('click', function() {
   melaniaCac();
@@ -22,38 +21,52 @@ trumpBtn2.addEventListener('click', function() {
   trumpRange();
 });
 
-function comparePos(el1, el2) {
-  if (el1.posX - 1 == el2.posX && el1.posY == el2.posY) {return true; pose = 'left';}
-  if (el1.posX + 1 == el2.posX && el1.posY == el2.posY) {return true; pose = 'right';}
-  if (el1.posX == el2.posX && el1.posY - 1 == el2.posY) {return true; pose = 'bottom';}
-  if (el1.posX == el2.posX && el1.posY + 1 == el2.posY) {return true; pose = 'top';}
+function comparePos(el1, el2, el3, el4) {
+  // Retourne true si les deux elements se trouvent sur une case adjacente
+  // var pose contient le côté où se trouve l'élément comparé
+  if (el1.posX - 1 == el2.posX && el3.posY == el4.posY) {
+    pose = 'left';
+    return true;
+  }
+  if (el1.posX + 1 == el2.posX && el3.posY == el4.posY) {
+    pose = 'right';
+    return true;
+  }
+  if (el1.posX == el2.posX && el3.posY - 1 == el4.posY) {
+    pose = 'bottom';
+    return true;
+  }
+  if (el1.posX == el2.posX && el3.posY + 1 == el4.posY) {
+    pose = 'top';}
+    return true;
 }
 
 function melaniaCac() {
-  comparePos();
+  comparePos(posXMela, posXTrump, posYMela, posYTrump);
   if (true) {
-    data.trumpHP.value -= 20;
+    datas.hpTrump -= 20;
     if (pose == 'left') {
-      data.TrumpX.value--;
+      datas.posXtrump--;
     } else if (pose == 'right') {
-      data.TrumpX.value++;
+      datas.posXTrump++;
     } else if (pose == 'top') {
-      data.TrumpY.value--;
+      datas.posYMela--;
     } else {
-      data.TrumpY.value++;
+      datas.posYMela++;
     }
 
   } else {
+    // Faire l'anim d'action impossible
     melaniaBtn1.className = 'nope';
   }
 }
 
 function melaniaRange() {
-  for (var i = data.melaniaX.value - 2; i < data.melaniaX.value + 2; i++) {
-    myTable.rows[i].cells[data.melaniaY.value].className = "range";
+  for (var i = datas.melaniaX.value - 2; i < datas.melaniaX.value + 2; i++) {
+    myTable.rows[i].cells[datas.melaniaY.value].className = "range";
   }
-  for (var i = data.melaniaY.value - 2; i < data.melaniaXY.value + 2; i++) {
-    myTable.rows[data.melaniaX.value].cells[i].className = "range";
+  for (var i = datas.melaniaY.value - 2; i < datas.melaniaXY.value + 2; i++) {
+    myTable.rows[datas.melaniaX.value].cells[i].className = "range";
   }
 }
 
