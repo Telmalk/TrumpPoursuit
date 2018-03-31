0b658c80c7c06e3e8a9243d929949af91a139d1b
Clément Haller <33249109+Telmalk@users.noreply.github.com>
2018-03-31 02:45:25 +0200

    Add files via upload

---
+++ asset/js/data.js
@@ -0,0 +1,25 @@
+var generateDatas = function() {
+    var posXTrump = document.querySelector(".posXTrump");
+    var posYTrul = document.querySelector(".posYtrump");
+    var hpTrump = document.querySelector(".hpTrump");
+    var posXMela = document.querySelector(".posXMela");
+    var posYMela = document.querySelector(".posYMela");
+    var hpMela = document.querySelector(".hpMeka");
+    var finalPosX = document.querySelector(".finalPosX");
+    var finalPosY = document.querySelector(".finalPosY");
+
+    var datas = {
+        "posXTrump": posXTrump.value,
+        "posYTrump": posYTrump.value,
+        "hpTrump": hpTrump.value,
+        "posXMela": posXMela.value,
+        "posYMela": posYMela.value,
+        "hpMela": hpMela.value,
+        "finalPosX": finalPosX.value,
+        "finalPosY": finalPosY.value;
+        "turn": 3
+    }
+    return datas;
+}
+
+datas = generateDatas();
